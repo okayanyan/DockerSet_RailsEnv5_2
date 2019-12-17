43e38eb075e2964e06b8d86e0619eff327cbc7c2
okayanyan <okayan.dse@gmail.com>
2019-12-17 14:48:43 +0900

    add nginx container for web-server

--- README.md
+++ README.md
@@ -2,25 +2,68 @@
 
 <br>
 
+---
+
+## What's this
+
+-   This is the files for creating environment for development.
+-   if it is executed, 3 containers are created. for app(Ruby on Rails), for, web-server, for database.
+
+<br>
+
+---
+
 ## How to use
 
--  Download & install Docker & Docker compose.
+1.  Download & install Docker & Docker compose.
 
--  Set folder in working directory.
+2.  Create AWS account & setting. (The following should be setting at least）
 
--  Execute init file.
+    1.  IAM
+    2.  S3
+
+3.  Set folder in working directory.
+
+    ~~~
+    --- work_dir
+     |
+     -- set
+    ~~~
+
+4.  Create .env file
+
+    -   set/file/mysql/mysql.env
+
+        ~~~sh
+        MYSQL_ROOT_PASSWORD=""
+        ~~~
+
+    -   set/file/rails/rails.env
+
+        ~~~sh
+        MYSQL_ENCODING=""
+        MYSQL_USER=""
+        MYSQL_PASSWORD=""
+        MYSQL_DATABASE=""
+        AWS_ACCESS_KEY=""
+        AWS_SECRET_ACCESS_KEY=""
+        AWS_BUCKET=""
+        AWS_REGION=""
+        ~~~
+
+5.  Execute init file.
 
     ~~~sh
     source dockerfile_init.sh
     ~~~
 
-- Execute boot file.
+6.  Execute boot file.
 
     ~~~sh
     source dockerfile_boot.sh
     ~~~
 
--  If you want to stop, execute stop file.
+7.  If you want to stop, execute stop file.
 
     ~~~sh
     source dockerfile_stop.sh
--- dockerfile_init.sh
+++ dockerfile_init.sh
@@ -11,24 +11,30 @@ docker-compose build --no-cache
 docker-compose up  -d
 
 # init rails application
-\cp -f ./Gemfile ../../$fd_env/Gemfile
-\cp -f ./Gemfile.lock ../../$fd_env/Gemfile.lock
-docker-compose exec app cd $fdpath_app
-docker-compose exec app rails new . -d mysql -s
+cp -f ./rails/Gemfile ../../$fd_env/Gemfile
+cp -f ./rails/Gemfile.lock ../../$fd_env/Gemfile.lock
+docker-compose exec app /bin/bash -c "cd $fdpath_app"
+docker-compose exec app /bin/bash -c "rails new . -d mysql -s"
 # Create database
-\cp -f ./database.yml ../../$fd_env/config/database.yml
-docker-compose exec app rails db:create
+cp -f ./rails/database.yml ../../$fd_env/config/database.yml
+docker-compose exec app /bin/bash -c "rails db:create"
 # Use slim
-docker-compose exec app bundle exec erb2slim $fdpath_app/app/views/layouts/ --delete
+docker-compose exec app /bin/bash -c "bundle exec erb2slim $fdpath_app/app/views/layouts/ --delete"
 # Use bootstrap
 rm ../../$fd_env/app/assets/stylesheets/application.css
 touch ../../$fd_env/app/assets/stylesheets/application.scss
 echo '@import "bootstrap";' > ../../$fd_env/app/assets/stylesheets/application.scss
 echo -e '//= require jquery3 \n//= require popper \n//= require bootstrap-sprockets' >> ../../$fd_env/app/assets/javascripts/application.js
 # Set Japanese
-docker-compose exec app "wget https://raw.githubusercontent.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml"
+docker-compose exec app /bin/bash -c "wget https://raw.githubusercontent.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml"
 mv ../../$fd_env/ja.yml  ../../$fd_env/config/locales/ja.yml -f
 touch ../../$fd_env/config/initializers/locale.rb
 echo 'Rails.application.config.i18n.default_locale = :ja' > ../../$fd_env/config/initializers/locale.rb
+# Set unicorn file
+cp -f ./rails/unicorn.rb ../../$fd_env/config/unicorn.rb
+cp -f ./rails/unicorn.rake ../../$fd_env/lib/tasks/unicorn.rake
+docker-compose exec app /bin/bash -c "bundle exec rails unicorn:start"
 # after operation
 cd ../
+
+echo 'Finish to initialize.'
